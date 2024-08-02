7e530aa58868ab197641051afb0d4d53c07bd4c2
Andreas Gangsø <andreasgangso@gmail.com>
2024-08-02 23:44:07 +0200

    fix highscore

--- scripts/death_screen.gd
+++ scripts/death_screen.gd
@@ -6,14 +6,14 @@ func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 	
 func _ready():
-	if Verdier.points > Verdier.highscore:
-		Verdier.highscore = Verdier.points
-		Verdier.points = 0
-		
 	if OS.has_feature("web"):
 		JavaScriptBridge.eval("""
 		fetch(`https://gamesbot.u3.style/highscore/"""+str(Verdier.points)+"""?game=beandash&${window.location.search.substr(1)}`)
 		""")
+		
+	if Verdier.points > Verdier.highscore:
+		Verdier.highscore = Verdier.points
+		Verdier.points = 0
 	
 	$Control/Highscore.text = "Highscore: " + str(Verdier.highscore)
 
